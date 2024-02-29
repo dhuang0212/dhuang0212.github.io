c0a518bd7a6e7d0b2f3044dbada2474f019cc529
dhuang0212 <dhuang@KVNGDVD-Main-PC.localdomain>
2024-02-29 00:57:13 -0500

    Fixing

--- scripts.js
+++ scripts.js
@@ -1,16 +1,13 @@
-window.addEventListener('scroll', function() {
-  // Debounce this function for performance
-  if (this.timeout) {
-      clearTimeout(this.timeout);
-  }
-  this.timeout = setTimeout(function() {
-      var navbar = document.getElementById("navigation-bar");
-      var sticky = navbar.offsetTop;
+window.onscroll = function() {myFunction()};
 
-      if (window.pageYOffset >= sticky) {
-          navbar.classList.add("sticky");
-      } else {
-          navbar.classList.remove("sticky");
-      }
-  }, 100);
-});
+var navbar = document.getElementById("navigation-bar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}

c4bca4a99576a9e5083dc1168a3ec45448b32ff5
dhuang0212 <dhuang@KVNGDVD-Main-PC.localdomain>
2024-02-29 01:02:57 -0500

    Revert "Fixing"
    This reverts commit c0a518bd7a6e7d0b2f3044dbada2474f019cc529.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navigation-bar");
-
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+window.addEventListener('scroll', function() {
+  // Debounce this function for performance
+  if (this.timeout) {
+      clearTimeout(this.timeout);
   }
-}
+  this.timeout = setTimeout(function() {
+      var navbar = document.getElementById("navigation-bar");
+      var sticky = navbar.offsetTop;
+
+      if (window.pageYOffset >= sticky) {
+          navbar.classList.add("sticky");
+      } else {
+          navbar.classList.remove("sticky");
+      }
+  }, 100);
+});
